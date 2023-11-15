101b184114f206bc88f339e78d09911c74fc42a0
Andrey <kedr_club@mail.ru>
2023-11-15 15:53:15 +0300

    feat: adds pagination logic

--- src/components/ListColumn/ListColumn.tsx
+++ src/components/ListColumn/ListColumn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ListColumnProps } from 'src/components/ListColumn/ListColumn.types';
 import Task from 'src/components/Task/Task';
@@ -9,18 +10,29 @@ import Pagination from 'components/Pagination/Pagination';
 
 const ListColumn = ({ label, isComplited }: ListColumnProps) => {
   const tasks = useAppSelector((state) => state.todo.tasks);
+  const [limit, setLimit] = useState<number>(3);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
   const dispatch = useAppDispatch();
 
   const handleClick = () => {
     dispatch(addCurrentTask(true));
   };
 
+  const taskList: ITask[] = tasks.filter(
+    (el: ITask) => !el.isCompleted === isComplited && typeof el.isImportant === 'boolean'
+  );
+
+  const currentList = taskList
+    .map((task: ITask) => <Task {...task} key={task.id} />)
+    .reverse()
+    .slice((currentPage - 1) * limit, currentPage * limit);
+
   return (
     <li className="todos__item">
       <div className="item__head">
         <h2 className="item__title">
-          {label} (
-          {tasks.filter((el: ITask) => !el.isCompleted === isComplited && typeof el.isImportant === 'boolean').length})
+          {label} ({taskList.length})
         </h2>
         {label === 'To do' && (
           <Link className="item__add" to={'/todoform'} onClick={handleClick}>
@@ -29,13 +41,14 @@ const ListColumn = ({ label, isComplited }: ListColumnProps) => {
           </Link>
         )}
       </div>
-      <ul className="item__tasks">
-        {tasks
-          .filter((el: ITask) => !el.isCompleted === isComplited && typeof el.isImportant === 'boolean')
-          .map((task: ITask) => <Task {...task} key={task.id} />)
-          .reverse()}
-      </ul>
-      <Pagination />
+      <ul className="item__tasks">{currentList}</ul>
+      <Pagination
+        taskList={taskList}
+        limit={limit}
+        setLimit={setLimit}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </li>
   );
 };
