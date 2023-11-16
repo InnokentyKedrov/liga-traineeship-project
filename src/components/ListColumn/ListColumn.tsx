e56da55f32455d523ed42d90c71f8ed7cd461fb5
Andrey <kedr_club@mail.ru>
2023-11-16 14:17:52 +0300

    feat: changed pagination to mui styled

--- src/components/ListColumn/ListColumn.tsx
+++ src/components/ListColumn/ListColumn.tsx
@@ -1,32 +1,61 @@
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import { styled } from '@mui/material/styles';
 import { ListColumnProps } from 'src/components/ListColumn/ListColumn.types';
 import Task from 'src/components/Task/Task';
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
 import { addCurrentTask } from 'src/redux/slices/taskSlice';
 import { ITask } from 'src/types';
 import 'src/components/ListColumn/ListColumn.css';
-import Pagination from 'components/Pagination/Pagination';
 
 const ListColumn = ({ label, isComplited }: ListColumnProps) => {
-  const tasks = useAppSelector((state) => state.todo.tasks);
-  const [limit, setLimit] = useState<number>(3);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
   const dispatch = useAppDispatch();
+  const tasks = useAppSelector((state) => state.todo.tasks);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [displayHeight, setDisplayHeight] = useState<number>(window.innerHeight);
+
+  const taskLimit = Math.floor((displayHeight - 460) / 120) + 1;
+
+  const taskList: ITask[] = tasks.filter(
+    (el: ITask) => !el.isCompleted === isComplited && typeof el.isImportant === 'boolean'
+  );
+  const pages = Math.ceil(taskList.length / taskLimit);
 
   const handleClick = () => {
     dispatch(addCurrentTask(true));
   };
 
-  const taskList: ITask[] = tasks.filter(
-    (el: ITask) => !el.isCompleted === isComplited && typeof el.isImportant === 'boolean'
-  );
-
   const currentList = taskList
     .map((task: ITask) => <Task {...task} key={task.id} />)
     .reverse()
-    .slice((currentPage - 1) * limit, currentPage * limit);
+    .slice((currentPage - 1) * taskLimit, currentPage * taskLimit);
+
+  const handleChange = (event: ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  };
+
+  const StyledPaginationItem = styled(PaginationItem)(() => ({
+    borderColor: 'var(--second-color)',
+    '&.Mui-selected': {
+      backgroundColor: 'var(--second-color)',
+      '&:hover': {
+        color: '#fff',
+      },
+    },
+    '&:hover': {
+      color: '#fff',
+    },
+  }));
+
+  useEffect(() => {
+    const handleResizeWindow = () => setDisplayHeight(window.innerHeight);
+    window.addEventListener('resize', handleResizeWindow);
+    return () => {
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+  });
 
   return (
     <li className="todos__item">
@@ -43,11 +72,12 @@ const ListColumn = ({ label, isComplited }: ListColumnProps) => {
       </div>
       <ul className="item__tasks">{currentList}</ul>
       <Pagination
-        taskList={taskList}
-        limit={limit}
-        setLimit={setLimit}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
+        count={pages}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChange}
+        siblingCount={0}
+        renderItem={(item) => <StyledPaginationItem {...item} />}
       />
     </li>
   );
